949bf3a4e85d4a6214e83cc9895ec71e215344f6
bboworn <bowornphakz@gmail.com>
2024-03-13 20:16:50 +0700

    Responsive-Homepage-Update-1

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -11,10 +11,10 @@ function Homepage() {
         />
       </div>
 
-      <div className="flex justify-center gap-10 mx-auto h-fit">
-        <div className="flex flex-col w-[363px] h-96">
-          <p className="font-bold text-8xl">2024</p>
-          <p className="font-bold text-5xl">Collection</p>
+      <div className="flex flex-col md:flex-row justify-center gap-10 mx-auto h-fit mb-16">
+        <div className="flex flex-col w-full md:w-[363px] h-96">
+          <p className="font-bold text-4xl md:text-8xl">2024</p>
+          <p className="font-bold text-3xl md:text-5xl">Collection</p>
           <p className="text-base">
             Step into a world of winter elegance and style with our latest
             Winter Collection. As temperatures drop, our curated selection of
@@ -28,11 +28,11 @@ function Homepage() {
         </div>
         <div className="relative">
           <img
-            className="w-[575px] h-[500px] object-cover bg-gradient-to-t from-slate-900 to-transparent "
+            className="w-full md:w-[575px] h-[500px] object-cover bg-gradient-to-t from-slate-900 to-transparent "
             src="./src/assets/img2.jpg"
           ></img>
           <div className="absolute bottom-4 px-4 flex flex-col items-center text-white ">
-            <p className="font-bold text-2xl">Cozy Breeze</p>
+            <p className="font-bold text-xl md:text-2xl">Cozy Breeze</p>
             <p className="font-normal text-base">
               Embrace the season with our carefully curated selection of
               garments, each piece thoughtfully designed to blend fashion and
@@ -46,40 +46,26 @@ function Homepage() {
         </div>
         <div className="relative">
           <img
-            className="min-w-[575px] h-[500px] object-cover bg-gradient-to-t from-slate-900 to-transparent "
-            src="./src/assets/img2.jpg"
+            className="w-full md:min-w-[575px] h-[500px] object-cover bg-gradient-to-t from-slate-900 to-transparent "
+            src="./src/assets/img3.jpg"
           ></img>
           <div className="absolute bottom-4 px-4 flex flex-col items-center text-white ">
-            <p className="font-bold text-2xl">Cozy Breeze</p>
+            <p className="font-bold text-xl md:text-2xl">Flexi Move</p>
             <p className="font-normal text-base">
-              Embrace the season with our carefully curated selection of
-              garments, each piece thoughtfully designed to blend fashion and
-              functionality. From cozy knits to elegant outerwear, our
-              collection invites you to indulge in the allure of winter fashion.
+              Step into a world where fashion meets functionality with our
+              latest Sneaker Collection. Designed for those who appreciate the
+              perfect fusion of style and comfort, our curated selection of
+              sneakers is a celebration of urban chic.
             </p>
             <button className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded">
               View more
             </button>
           </div>
         </div>
-        {/* <div>
-          <img className="size-96" src="./src/assets/img3.jpg"></img>
-          <p className="font-bold text-2xl">Flexi Move</p>
-          <p className="font-normal text-base">
-            Step into a world where fashion meets functionality with our latest
-            Sneaker Collection. Designed for those who appreciate the perfect
-            fusion of style and comfort, our curated selection of sneakers is a
-            celebration of urban chic.
-          </p>
-          <button className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded">
-            View more
-          </button>
-        </div> */}
       </div>
-
-      <div>
-        Feature
-        <div className="grid grid-cols-4 gap-10 mx-auto w-fit">
+      <div className="flex flex-col items-center px-4 font-bold mb-16">
+        <p className="text-[32px]">Feature Products</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 mx-auto w-full md:w-fit">
           <ProductCard />
         </div>
       </div>
